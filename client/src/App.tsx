0244fe9b66916cc9d0c03b4ea6dffcd3ae218434
Dharmesh Sharma <dharmesh_sharma15@live.com>
2024-03-14 23:20:25 +0530

    adding nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from @/scenes/navbar";
+
 function App() {
-  return <></>;
+  const theme = useMemo(() => createTheme(), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/predictions" element={<div>prediction page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
